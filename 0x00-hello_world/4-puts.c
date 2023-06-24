e63513a97247f2e3b5f0a6c2342268c330aca445
Ezekiel Isang <Ecrissang7@gmail.com>
2023-06-24 16:36:22 -0700

    low level programming hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
-include <stdio.h>
+#include <stdio.h>
+
 /**
- * main - 4-puts.c
- * description - Write a C program that prints exactly
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main - 5-printf.c
- * description - Write a C program that prints exactly with proper grammar,
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
