8f778966394bf5c83cfcc268aab6e7a611dc4583
Ezekiel Isang <Ecrissang7@gmail.com>
2023-08-01 16:22:14 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,6 @@
-#include "lists.h"
-#include <stdio.h>
+<<<<<<< HEAD
+#include"lists.h"
+#include<stdio.h>
 
 /**
  * print_listint - print elements of linkedlist
@@ -10,14 +11,36 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nbr_nodes = 0;
-	const listint_t *current = h;
+size_t i = 0;
+while (h)
+{
+printf("%i\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
+=======
+#include "lists.h"
+#include <stdio.h>
 
-	while (current != NULL)
+/**
+ * print_listint - Print elements of a singly linked list.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *tp;
+	unsigned int cntr = 0;
+
+	tp = h;
+	while (tp)
 	{
-		printf("%i\n", current->n);
-		current = current->next;
-		nbr_nodes++;
+		printf("%d\n", tp->n);
+		cntr++;
+		tp = tp->next;
 	}
-	return (nbr_nodes);
+	return (cntr);
+>>>>>>> 350df5726b6cfac0e65feafeaf746eac4dc11f0f
 }
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,31 +1,21 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint2 - a function that frees the list and sets head to NULL
- *
- * @head: pointer to the first node
- *
- * Return: nothing
-*/
+ * free_listint2 - Frees a list.
+ * @head: Address of the first node of a list.
+ **/
+
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *temp;
+	listint_t *temp;
 
-	if (head != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		/*set head addr to current*/
-		current = *head;
-
-		/*iterate through the whole list*/
-		/*while setting the current node to temp*/
-		while ((temp = current) != NULL)
-		{
-			/*set next node to curretnt*/
-			current = current->next;
-			/*free temp, that is the current node*/
-			free(temp);
-		}
-
-		*head = NULL;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 }
